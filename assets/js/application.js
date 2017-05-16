b4f6f04ba1fa30ad144db2fae1bc307196bd4b8f
Francisco Zanatta - 'Xikaos <francisco@organicadigital.com>
2017-05-15 21:26:58 -0300

    Objeto com dados dos eps. estruturados/validados até pg 16.

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,26 +1,64 @@
 //Create a root in the scope chain
 var app = app || {};
 
-app.do_request_old = function() {
-	var page_num = $('#page_num').val();
-	var xhr = new XMLHttpRequest();
-	xhr.open("get", "http://localhost/lib/get.php?page_num=".concat(page_num));
-	xhr.send();
-}
-
 app.do_request = function() {
-	var page_num = $('#page_num').val();
-	var url = "http://localhost/lib/get.php?page_num=".concat(page_num);
+	page_num = $('#page_num').val();
+	url = "http://localhost/lib/get.php?page_num=".concat(page_num);
 	$.ajax(url, {
+		dataType: 'text',
 		success: function(page){
-			$doc = jQuery.parseHTML(page);
-		} 
+			app.scrape(page);
+		}, 
 		error: function(){
-			console.log("Could not get " + url.);
+			console.log("Could not get " + url);
 		}
 	})
 }
 
+app.scrape = function(page) {
+	data = [];
+	episodes = $('.post', page);
+	for(i = 0; i < episodes.length; i++){
+		episode_tags = episodes[i].children; 
+		title = episode_tags[0].innerText;
+		date = episode_tags[1].innerText;
+		setlist = episode_tags[2].innerText;
+	  download = $('a.download_link',episode_tags).attr('href');
+		if(!download || download.typeof === 'undefined' ){
+			// download = $('p:nth-child(3) > a',episode_tags).attr('href');
+			download = $("a[title='MP3 Download']",episode_tags).attr('href');
+		} 
+
+		item = {
+			"title": title,
+			"date": date,
+			"setlist": setlist,
+			"download": download,  
+		};
+		data.push(item);
+	}
+	app.validates(data);
+}
+
+app.validates = function(data){
+	data.forEach(function(item){
+		if(!item.title || item.title.typeof === 'undefined' ){
+			console.log('miss title ' + item.title);
+		} 
+		else if((!item.date || item.date.typeof === 'undefined' )){
+			console.log('miss date ' + item.title);
+		}
+		else if((!item.download || item.download.typeof === 'undefined' )){
+			console.log('miss download ' + item.title);
+		}
+		else if((!item.setlist || item.setlist.typeof === 'undefined' )){
+			console.log('miss setlist ' + item.title);
+		} else {
+			console.log('OK!');
+		}
+		
+	});
+}	
 
 //Doing this way you polute the global scope
 // do_request = function(page){
--- lib/get.php
+++ lib/get.php
@@ -1,13 +1,16 @@
 <?php
 $page_num = $_GET['page_num'];
-//Initialize curl object
 $curl = curl_init();
-//Set curl url
+
 curl_setopt($curl, CURLOPT_URL, "http://podcast.hernancattaneo.com/page/{$page_num}");
-//
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, false);
 curl_setopt($curl,CURLOPT_HEADER, false); 
 
 $page = curl_exec($curl);
 
-echo $page;
+if(!$page){
+	echo($curl_errors);
+} else {
+	curl_close($curl);
+	return $page;
+}
