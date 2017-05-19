30f4b1ce84c930783c826bbb2478ed3d31a0f3eb
Francisco Zanatta - 'Xikaos <francisco@organicadigital.com>
2017-05-18 23:18:08 -0300

    Database setup/model ok.

--- lib/post.php
+++ lib/post.php
@@ -18,7 +18,7 @@ function db_setup($host, $user, $pass, $database){
 	}
 }
 
-function t_exists($conn, $table){
+function tb_exists($conn, $table){
 	$exists = mysqli_query($conn, "
 		SELECT table_name
 		FROM information_schema.tables 
@@ -35,21 +35,37 @@ $database = 'ntasd';
 $conn = db_setup($host, $user, $pass, $database);
 
 //	CHECK IF TABLES EXIST
-if(t_exists($conn,'episodes')){
-	echo('exists');
+if(tb_exists($conn,'Pages')){
+	echo('Pages exists');
+} 
+else {
+	$pages_create = 
+		"CREATE TABLE Pages (
+			page_id INT NOT NULL,
+			PRIMARY KEY (page_id));";
+	mysqli_query($conn, $pages_create);
+	echo(mysqli_error($conn));
+}
+
+if(tb_exists($conn,'Episodes')){
+	echo('Episodes exist!');
 } 
 else {
 	$create = 
-		"CREATE TABLE episodes (
-			`id` INT NOT NULL,
-			`date` VARCHAR(45) NULL,
-			`download` VARCHAR(45) NULL,
-			`setlist` VARCHAR(45) NULL,
-			`title` VARCHAR(45) NULL,
-			 `mp3` BLOB NULL,
-			 PRIMARY KEY (`id`));";
+		"CREATE TABLE Episodes (
+		id INT NOT NULL,
+		page_id INT NOT NULL,
+		date VARCHAR(45) NULL,
+		download VARCHAR(45) NULL,
+		setlist VARCHAR(45) NULL,
+		title VARCHAR(45) NULL,
+		mp3 BLOB NULL,
+		PRIMARY KEY (id),
+		FOREIGN KEY (page_id) REFERENCES Pages(page_id));";
   mysqli_query($conn, $create);
+  echo(mysqli_error($conn));
 }
+//var_dump($_POST);
 
 
 
